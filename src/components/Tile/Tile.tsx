bfed93ce96a96a251fc23de56db8d8c764924044
sungwon-cha <mygenie3@naver.com>
2023-01-08 20:49:48 +0900

    fix

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
+// import { yellow, orange, red, blue, green, grey } from '@ant-design/colors';
 import styles from './Tile.module.scss';
 // import { Tile as TileClass } from '../../lib/GameBase';
 import { Color } from '../../lib/constants';
-// import { isAnyArrayBuffer } from 'util/types';
-// import
 
-// const type
-// type Aaa = (typeof  Game)
+// https://colorswall.com/palette/171
+// const colors = {
+//   [Color.White]: grey,
+//   [Color.Yellow]: yellow,
+//   [Color.Orange]: orange,
+//   [Color.Red]: red,
+//   [Color.Green]: green,
+//   [Color.Blue]: blue,
+// };
 
-// type AnimalConstructor<T> = new (name: string) => T
+const colors = {
+  // [Color.White]: `rgb(255, 255, 255)	`,
+  // [Color.Yellow]: `rgb(255, 213, 0)	`,
+  // [Color.Orange]: `rgb(255, 88, 0)`,
+  // [Color.Red]: `rgb(183, 18, 52)`,
+  // [Color.Green]: `rgb(0, 155, 72)`,
+  // [Color.Blue]: `rgb(0, 70, 173)`,
 
-// let a : Aaa
+  [Color.White]: `rgb(255, 255, 255)`,
+  [Color.Yellow]: `rgb(255, 225, 2)`,
+  [Color.Orange]: `rgb(240, 124, 0)`,
+  [Color.Red]: `rgb(229, 0, 38)`,
+  [Color.Green]: `rgb(4, 162, 57)`,
+  [Color.Blue]: `rgb(0, 82, 162)`,
+};
 
-// type Aaa = typeof TileClass;
-
-// const a: Aaa;
-// TileClass.
-// TileClass.
-
-interface ITileProps {
+interface TileProps {
   /**
    * Prop Description
    */
@@ -33,7 +45,7 @@ interface ITileProps {
 /**
  * Component Description
  */
-function Tile(props: ITileProps) {
+function Tile(props: TileProps) {
   if (typeof props.x === 'number' && typeof props.y === 'number') {
     // const handleClick = () => {
     //   alert(`${props.x} ${props.y}`);
@@ -42,17 +54,18 @@ function Tile(props: ITileProps) {
       <div
         className={styles.root}
         style={{
-          background: props.color,
-          top: props.y * 50,
-          left: props.x * 50,
+          backgroundColor: colors[props.color],
+          borderColor: colors[props.color],
+          top: props.y * 55,
+          left: props.x * 55,
         }}
         onClick={() => {
           props.onClick(props.x, props.y);
         }}
       >
-        <h3>
+        {/* <h3>
           {props.x} / {props.y}
-        </h3>
+        </h3> */}
       </div>
     );
   }
